0bf167ac0ddba0153ac6cd1dd13c89aacc88b9cf
dennisomandi <ormadydennis@gmail.com>
2022-10-01 12:47:28 +0300

    deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 # Fabfile to generates a .tgz archive from the contents of web_static.
 import os.path
 from datetime import datetime
-from fabric.api import *
+from fabric.api import local
 
 
 def do_pack():
